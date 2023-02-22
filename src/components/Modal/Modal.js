4f87c8859bbe24dc8b6c59d09815469028b74308
Pankumi <111710215+Pankumi@users.noreply.github.com>
2023-02-22 02:11:05 +0200

    prettier

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import css from './Modal.module.css'
 
 
-export const Modal = ({ selectedImg, modalSwitch }) => {
+export const Modal = ({ selectedImg: {largeImageURL, tags}, modalSwitch }) => {
 
   useEffect( () => {
     window.addEventListener('keydown', handleEvent);
@@ -21,41 +21,8 @@ export const Modal = ({ selectedImg, modalSwitch }) => {
   return (
       <div className={css.Overlay} onClick={handleEvent}>
         <div className={css.Modal}>
-          <img src={selectedImg.largeImageURL} alt={selectedImg.tags} />
+          <img src={largeImageURL} alt={tags} />
         </div>
       </div>
     );
 }
-
-
-// export class Modal extends Component {
-
-//   componentDidMount(){
-//     window.addEventListener('keydown', this.handleEvent);
-//   };
-
-//   componentWillUnmount(){
-//     window.removeEventListener('reydown', this.handleEvent);
-//   };
-
-//   handleEvent = evt => {
-//       // console.log('keydown >>', evt.code );
-//       if( evt.code === 'Escape') {
-//         this.props.modalSwitch()
-//       }
-//       if( evt.target === evt.currentTarget ){
-//         this.props.modalSwitch()
-//       }
-//   };
-
-//   render() {
-//     const { largeImageURL, tags } = this.props.selectedImg
-//     return (
-//       <div className={css.Overlay} onClick={this.handleEvent}>
-//         <div className={css.Modal}>
-//           <img src={largeImageURL} alt={tags} />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
