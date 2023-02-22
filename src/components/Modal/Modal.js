03ae09ad10959fa2fe5a17baac1fd3f90a0c0bf0
Pankumi <111710215+Pankumi@users.noreply.github.com>
2023-02-22 02:04:33 +0200

    refactoring Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,34 +1,61 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import css from './Modal.module.css'
 
-export class Modal extends Component {
 
-  componentDidMount(){
-    window.addEventListener('keydown', this.handleEvent);
-  };
+export const Modal = ({ selectedImg, modalSwitch }) => {
 
-  componentWillUnmount(){
-    window.removeEventListener('reydown', this.handleEvent);
-  };
+  useEffect( () => {
+    window.addEventListener('keydown', handleEvent);
 
-  handleEvent = evt => {
-      // console.log('keydown >>', evt.code );
-      if( evt.code === 'Escape') {
-        this.props.modalSwitch()
-      }
-      if( evt.target === evt.currentTarget ){
-        this.props.modalSwitch()
+    return () => {
+      window.removeEventListener('reydown', handleEvent);
+    }
+  },[] );
+
+  const handleEvent = ({ code, target, currentTarget }) => {
+      if( code === 'Escape' || target === currentTarget) {
+        modalSwitch();
       }
   };
 
-  render() {
-    const { largeImageURL, tags } = this.props.selectedImg
-    return (
-      <div className={css.Overlay} onClick={this.handleEvent}>
+  return (
+      <div className={css.Overlay} onClick={handleEvent}>
         <div className={css.Modal}>
-          <img src={largeImageURL} alt={tags} />
+          <img src={selectedImg.largeImageURL} alt={selectedImg.tags} />
         </div>
       </div>
     );
-  }
 }
+
+
+// export class Modal extends Component {
+
+//   componentDidMount(){
+//     window.addEventListener('keydown', this.handleEvent);
+//   };
+
+//   componentWillUnmount(){
+//     window.removeEventListener('reydown', this.handleEvent);
+//   };
+
+//   handleEvent = evt => {
+//       // console.log('keydown >>', evt.code );
+//       if( evt.code === 'Escape') {
+//         this.props.modalSwitch()
+//       }
+//       if( evt.target === evt.currentTarget ){
+//         this.props.modalSwitch()
+//       }
+//   };
+
+//   render() {
+//     const { largeImageURL, tags } = this.props.selectedImg
+//     return (
+//       <div className={css.Overlay} onClick={this.handleEvent}>
+//         <div className={css.Modal}>
+//           <img src={largeImageURL} alt={tags} />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
